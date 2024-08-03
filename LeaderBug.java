3366c494f9da25a0dec74c19d0a4198957ba498a
minhngoc24 <167333030+minhngoc24@users.noreply.github.com>
2024-08-03 23:03:42 +0700

    Create LeaderBug.java

---
+++ LeaderBug.java
@@ -0,0 +1,11 @@
+    /** NAME: MINH NGOC LE
+     *  CLASS: COMP 142 (Section 2)
+     *  Description: Create a graphical animation of a ladybug dance party
+     */
+    package project5;
+
+   public interface LeaderBug {
+
+       public abstract DanceStep getLastStep();
+   }
+   
